b2a791a510c3f6cea5319b84333efa457532e238
hazzard993 <hazzard993@gmail.com>
2019-02-10 08:34:58 +1000

    Restructure for NPM

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+// Typescript definitions for LÖVE 2D
+// Project: https://github.com/hazzard993/love-typescript-definitions
+// Definitions by: Brian Zengel <https://github.com/bczengel>,
+//                 Ilya Mochalov <https://github.com/chrootsu>,
+//                 Stepan Mikhaylyuk <https://github.com/stepancar>,
+//                 AJ Richardson <https://github.com/aj-r>,
+//                 Junyoung Clare Jang <https://github.com/ailrun>,
+//                 e-cloud <https://github.com/e-cloud>,
+//                 Georgii Dolzhykov <https://github.com/thorn0>,
+//                 Jack Moore <https://github.com/jtmthf>,
+//                 Dominique Rau <https://github.com/DomiR>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.6
+
+/// <reference no-default-lib="true"/>
+
+/// <reference path="./love.d.ts" />
+/// <reference path="./array.ext.d.ts" />
+/// <reference path="./coroutine.d.ts" />
+/// <reference path="./debug.d.ts" />
+/// <reference path="./function.ext.d.ts" />
+/// <reference path="./global.d.ts" />
+/// <reference path="./io.d.ts" />
+/// <reference path="./lib.d.ts" />
+/// <reference path="./missing.d.ts" />
+/// <reference path="./math.d.ts" />
+/// <reference path="./metatable.d.ts" />
+/// <reference path="./modules.d.ts" />
+/// <reference path="./os.d.ts" />
+/// <reference path="./string.d.ts" />
+/// <reference path="./string.ext.d.ts" />
+/// <reference path="./table.d.ts" />
