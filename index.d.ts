df2ef4fc782c08e8cd602eab39cb9e25cae4d055
hazzard993 <hazzard993@gmail.com>
2019-02-24 09:30:17 +1000

    Removed header from index

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,3 @@
-// Typescript definitions for LÖVE 2D
-// Project: https://github.com/hazzard993/love-typescript-definitions
-// Definitions by: Brian Zengel <https://github.com/bczengel>,
-//                 Ilya Mochalov <https://github.com/chrootsu>,
-//                 Stepan Mikhaylyuk <https://github.com/stepancar>,
-//                 AJ Richardson <https://github.com/aj-r>,
-//                 Junyoung Clare Jang <https://github.com/ailrun>,
-//                 e-cloud <https://github.com/e-cloud>,
-//                 Georgii Dolzhykov <https://github.com/thorn0>,
-//                 Jack Moore <https://github.com/jtmthf>,
-//                 Dominique Rau <https://github.com/DomiR>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.6
-
 /// <reference path="./love.d.ts" />
 /// <reference path="./coroutine.d.ts" />
 /// <reference path="./debug.d.ts" />
