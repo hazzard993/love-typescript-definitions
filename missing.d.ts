964bc318164445f938ddcb10dda13eac808015c5
hazzard993 <hazzard993@gmail.com>
2019-02-24 10:43:50 +1000

    Variant is an any type

--- missing.d.ts
+++ missing.d.ts
@@ -1,15 +1,14 @@
 // Other definitions that need to be included alongside love.d.ts.
 // Needs l2d.lib.d.ts to work and l2d.lib.dts needs this to work.
 
-type RestartString = "restart"
-type VertexWinding = "cw" | "ccw"
-type CullMode = "back" | "front" | "none"
-type LightUserData = number
-type LuaTable = Object
-type Variant = LuaTable | number | string | boolean
-type TextureType = "2d" | "array" | "cube" | "volume"
-type ShaderVariableType = any 	// Unknown type :(
+type RestartString = "restart";
+type VertexWinding = "cw" | "ccw";
+type CullMode = "back" | "front" | "none";
+type LightUserData = number;
+type Variant = any;
+type TextureType = "2d" | "array" | "cube" | "volume";
+type ShaderVariableType = any;
 
 // Comment out the below code if not including l2d.lib.d.ts
-type PixelFormat = CanvasFormat
-type CanvasMipmapMode = "none" | "auto" | "manual"
+type PixelFormat = CanvasFormat;
+type CanvasMipmapMode = "none" | "auto" | "manual";
