46807fb1c618400e8ec46316cfd9eabf21f01a9a
hazzard993 <hazzard993@gmail.com>
2019-03-03 14:25:22 +1000

    Added love.thread

--- include/modules/graphics/graphics_spec.ts
+++ include/modules/graphics/graphics_spec.ts
@@ -37,25 +37,6 @@ canvas.setWrap;
 
 /**
  * Documented at
- * https://love2d.org/wiki/Channel
- * 2019/03/03
- */
-let channel: Channel;
-channel.clear;
-channel.demand;
-channel.getCount;
-channel.hasRead;
-channel.peek;
-channel.performAtomic;
-channel.pop;
-channel.push;
-channel.supply;
-channel.release;
-channel.type;
-channel.typeOf;
-
-/**
- * Documented at
  * https://love2d.org/wiki/Drawable
  * 2019/03/03
  */
