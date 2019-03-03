8b12d7ecbfbb75dc634d918875eac3d9934c4c80
hazzard993 <hazzard993@gmail.com>
2019-03-03 14:21:13 +1000

    Added love.system

---
+++ include/modules/system/enums/PowerState.d.ts
@@ -0,0 +1,5 @@
+/**
+ * The basic state of the system's power supply.
+ * @link [PowerState](https://love2d.org/wiki/PowerState)
+ */
+declare type PowerState = 'unknown' | 'battery' | 'nobattery' | 'charging' | 'charged';
