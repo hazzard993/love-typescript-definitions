8998b6e1fa3caf82a71230381c6fbf4007fbd82b
hazzard993 <hazzard993@gmail.com>
2019-03-03 14:18:32 +1000

    Added love.sound

--- include/modules/audio/audio_spec.ts
+++ include/modules/audio/audio_spec.ts
@@ -8,21 +8,6 @@ let timeUnit: TimeUnit;
 
 /**
  * Documented at
- * https://love2d.org/wiki/Decoder
- */
-let decoder: Decoder;
-decoder.decode;
-decoder.getBitDepth;
-decoder.getChannelCount;
-decoder.getChannels;
-decoder.getDuration;
-decoder.getSampleRate;
-decoder.release;
-decoder.type;
-decoder.typeOf;
-
-/**
- * Documented at
  * https://love2d.org/wiki/RecordingDevice
  * Obtained 2019/03/02
  */
@@ -39,27 +24,6 @@ recordingDevice.getSampleRate;
 recordingDevice.isRecording;
 recordingDevice.start;
 recordingDevice.stop;
-
-/**
- * Documented at
- * https://love2d.org/wiki/SoundData
- */
-let soundData: SoundData;
-soundData.clone;
-soundData.getPointer;
-soundData.getSize;
-soundData.getString;
-soundData.release;
-soundData.type;
-soundData.typeOf;
-soundData.getBitDepth;
-soundData.getChannelCount;
-soundData.getChannels;
-soundData.getDuration;
-soundData.getSample;
-soundData.getSampleCount;
-soundData.getSampleRate;
-soundData.setSample;
 
 /**
  * Documented at
