f4c052e34a4c175f66ff89116e850f528f2018e2
hazzard993 <hazzard993@gmail.com>
2019-03-03 22:22:42 +1000

    Neatened up enums

--- include/modules/data/enums/EncodeFormat.d.ts
+++ include/modules/data/enums/EncodeFormat.d.ts
@@ -1,5 +1,6 @@
 /**
  * Return type of various data-returning functions.
  * @link [EncodeFormat](https://love2d.org/wiki/EncodeFormat)
+ * @snice 11.0
  */
 declare type EncodeFormat = "base64" | "hex";
