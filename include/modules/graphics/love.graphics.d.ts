607372705f9a5f792913303ca403354ef614c810
hazzard993 <hazzard993@gmail.com>
2019-06-20 16:08:44 +1000

    Remove Stats from global declarations

--- include/modules/graphics/love.graphics.d.ts
+++ include/modules/graphics/love.graphics.d.ts
@@ -661,7 +661,40 @@ declare namespace love {
          * @return stats, A table with the following fields:
          * @link [love.graphics.getStats](https://love2d.org/wiki/love.graphics.getStats)
          */
-        export function getStats(): Stats;
+        export function getStats(): {
+            /**
+             * The number of draw calls made so far during the current frame.
+             */
+            drawcalls: number;
+            /**
+             * The number of times the active Canvas has been switched so far during the current frame.
+             */
+            canvasswitches: number;
+            /**
+             * The estimated total size in bytes of video memory used by all loaded Images, Canvases, and Fonts.
+             */
+            texturememory: number;
+            /**
+             * The number of Image objects currently loaded.
+             */
+            images: number;
+            /**
+             * The number of Canvas objects currently loaded.
+             */
+            canvases: number;
+            /**
+             * The number of Font objects currently loaded.
+             */
+            fonts: number;
+            /**
+             * The number of times the active Shader has been changed so far during the current frame.
+             */
+            shaderswitches: number;
+            /**
+             * The number of draw calls that were saved by LÖVE's automatic batching, since the start of the frame.
+             */
+            drawcallsbatched: number;
+        };
 
         /**
          * Gets whether stencil testing is enabled.
