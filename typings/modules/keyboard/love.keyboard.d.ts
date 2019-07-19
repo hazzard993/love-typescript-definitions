b9b951d2c6c2de3f44be7d348eaaaadcb20590ea
hazzard993 <hazzard993@gmail.com>
2019-07-20 07:46:08 +1000

    Omit love.keyword scancode and key description

--- typings/modules/keyboard/love.keyboard.d.ts
+++ typings/modules/keyboard/love.keyboard.d.ts
@@ -8,16 +8,6 @@ declare namespace love {
         /**
          * Gets the key corresponding to the given hardware scancode.
          *
-         *
-         * Unlike key constants, Scancodes are keyboard layout-independent. For example
-         * the scancode "w" will be generated if the key in the same place as the "w" key
-         * on an American keyboard is pressed, no matter what the key is labelled or what
-         * the user's operating system settings are.
-         *
-         *
-         * Scancodes are useful for creating default controls that have the same physical
-         * locations on on all systems.
-         *
          * @param scancode The scancode to get the key from.
          * @return key, The key corresponding to the given scancode, or "unknown" if the scancode doesn't map to a KeyConstant on the current system.
          * @link [love.keyboard.getKeyFromScancode](https://love2d.org/wiki/love.keyboard.getKeyFromScancode)
@@ -26,16 +16,6 @@ declare namespace love {
 
         /**
          * Gets the hardware scancode corresponding to the given key.
-         *
-         *
-         * Unlike key constants, Scancodes are keyboard layout-independent. For example
-         * the scancode "w" will be generated if the key in the same place as the "w" key
-         * on an American keyboard is pressed, no matter what the key is labelled or what
-         * the user's operating system settings are.
-         *
-         *
-         * Scancodes are useful for creating default controls that have the same physical
-         * locations on on all systems.
          *
          * @param key The key to get the scancode from.
          * @return scancode, The scancode corresponding to the given key, or "unknown" if the given key has no known physical representation on the current system.
