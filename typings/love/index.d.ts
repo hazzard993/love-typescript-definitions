1140f4a1eadcd3690b98806bd3e647382233579a
hazzard993 <hazzard993@gmail.com>
2020-04-05 11:16:45 +1000

    Move love types to types/

--- typings/love/index.d.ts
+++ typings/love/index.d.ts
@@ -1,9 +1,8 @@
 import "./types/Conf";
-import "./enums/Version";
+import "./types/Version";
+import "./types/LightUserData";
+import "./types/Type";
+import "./types/Types";
+import "./types/UtilityTypes";
 import "./callbacks";
 import "./functions";
-import "./LightUserData";
-import "./structs";
-import "./Type";
-import "./Types";
-import "./UtilityTypes";
