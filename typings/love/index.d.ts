0f5f8669706ce6b6ac35ef2c23e683683e58d95a
Jason McKenzie <hazzard993@gmail.com>
2025-11-30 12:41:39 +1000

    Remove non-existent UtilityTypes import #78

--- typings/love/index.d.ts
+++ typings/love/index.d.ts
@@ -3,6 +3,5 @@ import "./types/Version";
 import "./types/LightUserData";
 import "./types/Type";
 import "./types/Types";
-import "./types/UtilityTypes";
 import "./handlers";
 import "./functions";
